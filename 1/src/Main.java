cf1b08515caaafcf3e638078ebf28d72edb7233e
sergei <terribloup@gmail.com>
2024-11-12 13:00:10 +0300

    1 project

--- 1/src/Main.java
+++ 1/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("anton");
+        boolean varboolean = true;
+        byte varbyte = 1;
+        short varshort = 2;
+        int varint = 3;
+        long varlong = 4;
+        float varfloat = 5.0f;
+        double vardouble = 6.0;
+        char varchar = 'a';
+        System.out.println(varboolean + " " + varbyte + " " + varshort + " " + varint + " " + varlong + " " + varfloat
+                + " " + vardouble + " " + varchar);
     }
 }
