77213ac0a4942daea5319c564656b7c1d4bee74f
sergei <terribloup@gmail.com>
2024-11-13 20:53:19 +0300

    1 done

---
+++ src/main/java/module1/Ex1.java
@@ -0,0 +1,14 @@
+package module1;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        boolean varboolean = true;
+        byte varbyte = 1;
+        short varshort = 1;
+        int varint = 1;
+        long varlong = 1;
+        float varfloat = 1;
+        double vardouble = 1;
+        char varchar = 'a';
+    }
+}
