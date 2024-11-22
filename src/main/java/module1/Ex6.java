7bc3c92d69ed6f16610515af08fe9189fe5583dd
sergei <terribloup@gmail.com>
2024-11-22 22:32:46 +0300

    Ex6 done

---
+++ src/main/java/module1/Ex6.java
@@ -0,0 +1,9 @@
+package module1;
+
+import java.math.BigInteger;
+
+public class Ex6 {
+    public static void maxLongSqr() {
+        System.out.println(BigInteger.valueOf(Long.MAX_VALUE).multiply(BigInteger.valueOf(Long.MAX_VALUE)));
+    }
+}
