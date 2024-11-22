777ca4a8cad41aa4e367aaf9cbd4627641e5c98d
sergei <terribloup@gmail.com>
2024-11-22 20:26:32 +0300

    2 done

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,21 @@
 package org.example;
 
 public class App {
+    public static void maxMin(double array[]) {
+        double max = array[0], min = array[0];
+        for (int i = 1; i < array.length; ++i) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        System.out.println("min: " + min + "\nmax: " + max);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        double a[] = {1, 45, 34, 5, 54, 6, 756, 73, 46, -1};
+        maxMin(a);
     }
 }
