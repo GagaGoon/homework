d8b1d10fabe498be2756171100128bc7529cf459
sergei <terribloup@gmail.com>
2024-11-22 19:53:00 +0300

    12 done

--- src/main/java/module1/Ex10.java
+++ src/main/java/module1/Ex10.java
@@ -4,11 +4,11 @@ import java.math.BigInteger;
 
 public class Ex10 {
     public static BigInteger factorial(int n) {
-        if(n<1){
+        if (n < 1) {
             return BigInteger.valueOf(-1);
         }
         BigInteger result = BigInteger.ONE;
-        for(int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
             result = result.multiply(BigInteger.valueOf(i));
         }
         return result;
