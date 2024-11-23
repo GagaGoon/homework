e2cfdaec27219b7670c593a71e2e6ea3a8559a43
sergei <terribloup@gmail.com>
2024-11-23 17:58:45 +0300

    Ex18 done

--- src/main/java/module1/Ex17.java
+++ src/main/java/module1/Ex17.java
@@ -2,6 +2,7 @@ package module1;
 
 public class Ex17 {
     public static void getSubArrayBetween(int[] numbers, int start, int end) {
+        System.out.println("[ ");
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] > start && numbers[i] < end) {
                 System.out.println(numbers[i]);
