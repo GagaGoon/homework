7eb81b812c3c7cc1bab4ea0852ea5c8f59527304
sergei <terribloup@gmail.com>
2024-12-08 17:18:57 +0300

    Ex29 first version done

--- src/main/java/module1/Ex29.java
+++ src/main/java/module1/Ex29.java
@@ -5,20 +5,14 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Ex29 {
-    public static int characterFind(String[] roles, String neededRole) {
-        int i = 0;
-        while (!neededRole.equals(roles[i])) {
-            ++i;
-        }
-        return i;
-    }
 
     public static String charactersLines(String[] roles, String[] textLines) {
         //создаем массив ролей и соответствующих им реплик
         StringBuilder[][] charactersLinesSBArray = new StringBuilder[2][roles.length];
+
         for (int i = 0; i < roles.length; ++i) {
             charactersLinesSBArray[0][i] = new StringBuilder(roles[i]);
-            charactersLinesSBArray[1][i] = new StringBuilder().append(roles[i] + ":");
+            charactersLinesSBArray[1][i] = new StringBuilder(roles[i] + ":");
         }
 
 
@@ -34,11 +28,16 @@ public class Ex29 {
 
 
             //выделяем новую реплику
-            String newReplica = new String("\n" + i + ")" + textLines[i].substring(characterMatcher.end() + 1));
+            String newReplica = new String("\n" + (i + 1) + ")" + textLines[i].substring(characterMatcher.end() + 1));
 
 
-            //находим нужную роль
-            int positionInRoles = Arrays.binarySearch(charactersLinesSBArray, characterMatcher.group());
+            //находим позицию нужной роли
+            int positionInRoles;
+            for (positionInRoles = 0; positionInRoles < roles.length; ++positionInRoles) {
+                if (characterMatcher.group().equals(charactersLinesSBArray[0][positionInRoles].toString())) {
+                    break;
+                }
+            }
 
 
             //записываем найденную реплику нужному персонажу
@@ -46,7 +45,24 @@ public class Ex29 {
         }
 
 
+        //записываем все реплики персонажей в один элемент
         StringBuilder charactersLinesSB = new StringBuilder();
+        for (int i = 0; i < roles.length; ++i) {
+            charactersLinesSB.append("\n\n").append(charactersLinesSBArray[1][i]);
+        }
+
         return charactersLinesSB.toString();
+    }
+
+    public static void main(String[] args) {
+        String[] roles = {"Городничий", "Аммос Федорович", "Артемий Филиппович", "Лука Лукич"};
+        String[] textLines = {"Городничий: Я пригласил вас, господа, с тем, чтобы сообщить вам пренеприятное известие: к нам едет ревизор."
+                , "Аммос Федорович: Как ревизор?"
+                , "Артемий Филиппович: Как ревизор?"
+                , "Городничий: Ревизор из Петербурга, инкогнито. И еще с секретным предписаньем."
+                , "Аммос Федорович: Вот те на!"
+                , "Артемий Филиппович: Вот не было заботы, так подай!"
+                , "Лука Лукич: Господи боже! еще и с секретным предписаньем!"};
+        System.out.println(charactersLines(roles, textLines));
     }
 }
